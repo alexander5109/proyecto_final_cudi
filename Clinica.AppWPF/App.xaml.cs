461d82778c87ff6f2ae542f0250a449b2d8b1c97
ecthelion5109 <xanderseling@gmail.com>
2025-11-09 06:06:05 -0300

    sonido roto?

--- Clinica.AppWPF/App.xaml.cs
+++ Clinica.AppWPF/App.xaml.cs
@@ -1,56 +1,61 @@
-﻿using System.Windows;
-using System.Windows.Media;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
-namespace Clinica.AppWPF {
-	public partial class App : Application {
-		public static bool SoundOn = false; // DEFAULT MODE // true;
-		public static BaseDeDatosAbstracta BaseDeDatos;
-		public static bool UsuarioLogueado = false;
-		public static MediaPlayer Sonidito = new MediaPlayer();
+namespace Clinica.AppWPF;
+public partial class App : Application {
+	public static bool SoundOn = true;
+	public static BaseDeDatosAbstracta BaseDeDatos;
+	public static bool UsuarioLogueado = false;
+	public static MediaPlayer Sonidito = new();
 
-		// public void StyleButton_Click(object sender, System.Windows.RoutedEventArgs e) {
-		// Sonidito.Open(new Uri("PublishedResources\\sonidos\\uclick_jewel.wav", UriKind.Relative));
-		// Sonidito.Play();
-		// }
+	private static string GetSoundPath(string fileName) {
+		string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PublishedResources", "sonidos", fileName);
 
-		public void StyleButton_MouseEnter(object sender, System.Windows.RoutedEventArgs e) {
-			//Es muy molesto.
-			// Sonidito.Open(new Uri("PublishedResources\\sonidos\\uclicknofun.wav", UriKind.Relative));
-			if (SoundOn) {
-				// Sonidito.Open(new Uri("PublishedResources\\sonidos\\PIU!.wav", UriKind.Relative));
-				Sonidito.Open(new Uri("PublishedResources\\sonidos\\uclicknofun.wav", UriKind.Relative));
-				Sonidito.Play();
-			}
+		if (!File.Exists(fullPath))
+			throw new FileNotFoundException($"Sound file not found: {fullPath}");
+
+		return fullPath;
+	}
+
+	public void StyleButton_MouseEnter(object sender, RoutedEventArgs e) {
+		if (SoundOn)
+			PlaySound("uclicknofun.wav");
+	}
+
+	public static void PlayClickJewel() {
+		if (SoundOn)
+			PlaySound("uclick_jewel.wav");
+	}
+
+	private static void PlaySound(string fileName) {
+		try {
+			string path = GetSoundPath(fileName);
+			Sonidito.Open(new Uri(path, UriKind.Absolute));
+			Sonidito.Play();
+		} catch (Exception ex) {
+			MessageBox.Show($"Error reproduciendo sonido: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
 		}
+	}
 
-		static public void PlayClickJewel() {
-			if (SoundOn) {
-				// Sonidito.Open(new Uri("PublishedResources\\sonidos\\ULTRAPEEOOU!.wav", UriKind.Relative));
-				Sonidito.Open(new Uri("PublishedResources\\sonidos\\uclick_jewel.wav", UriKind.Relative));
-				Sonidito.Play();
-			}
+
+	public static bool TryParseHoraField(string campo) {
+		if (TimeOnly.TryParse(campo, out _)) {
+			return true;
+		} else {
+			return false;
 		}
+	}
 
 
-
-		public static bool TryParseHoraField(string campo) {
-			if (TimeOnly.TryParse(campo, out _)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-
-		public static void UpdateLabelDataBaseModo(Label label) {
-			if (App.BaseDeDatos is BaseDeDatosJSON) {
-				label.Content = "Modo JSON";
-			} else if (App.BaseDeDatos is BaseDeDatosSQL) {
-				label.Content = "Modo SQL";
-			} else {
-				label.Content = "Elegir DB Modo";
-			}
+	public static void UpdateLabelDataBaseModo(Label label) {
+		if (App.BaseDeDatos is BaseDeDatosJSON) {
+			label.Content = "Modo JSON";
+		} else if (App.BaseDeDatos is BaseDeDatosSQL) {
+			label.Content = "Modo SQL";
+		} else {
+			label.Content = "Elegir DB Modo";
 		}
 	}
 }
